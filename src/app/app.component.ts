1cb749ae6a53f59fd6a7301fd8e724e6ae085f26
Junaid Ali Hakro <junaid.hakro@systemsltd.com>
2023-09-06 17:21:53 +0500

    [UPDATE] status functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,50 @@ import { Task } from 'src/types';
 export class AppComponent {
   title = 'task-management-app';
 
-  tasksList: Task[] = [];
+  tasksList: Task[] = [
+    {
+      id: '1',
+      name: 'Task 1',
+      priority: 'low',
+      status: 'complete',
+      date: new Date(),
+    },
+    {
+      id: '2',
+      name: 'Task 2',
+      priority: 'neutral',
+      status: 'incomplete',
+      date: new Date(),
+    },
+    {
+      id: '3',
+      name: 'Task 3',
+      priority: 'high',
+      status: 'incomplete',
+      date: new Date(),
+    },
+    {
+      id: '4',
+      name: 'Task 4',
+      priority: 'low',
+      status: 'incomplete',
+      date: new Date(),
+    },
+    {
+      id: '5',
+      name: 'Task 5',
+      priority: 'low',
+      status: 'incomplete',
+      date: new Date(),
+    },
+    {
+      id: '6',
+      name: 'Task 6',
+      priority: 'low',
+      status: 'incomplete',
+      date: new Date(),
+    },
+  ];
 
   addNewTask(task: Task) {
     this.tasksList.push(task);
@@ -18,5 +61,12 @@ export class AppComponent {
   deleteTask(id: string) {
     const index = this.tasksList.findIndex((task) => task.id === id);
     this.tasksList.splice(index, 1);
+  }
+
+  updateStatus(id: string) {
+    const index = this.tasksList.findIndex((task) => task.id === id);
+    const status =
+      this.tasksList[index].status === 'complete' ? 'incomplete' : 'complete';
+    this.tasksList[index] = { ...this.tasksList[index], status };
   }
 }
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -9,10 +9,18 @@ export class TaskListComponent {
   todayDate = new Date();
 
   @Input() taskList: Task[] = [];
+
   @Output()
   deleteTask: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  updateStatus: EventEmitter<string> = new EventEmitter<string>();
+
   deleteTaskHandler(id: string) {
     this.deleteTask.emit(id);
+  }
+
+  updateStatusHandler(id: string) {
+    this.updateStatus.emit(id);
   }
 }
